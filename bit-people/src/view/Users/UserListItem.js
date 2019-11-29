600b7e6cf5d0fb8dc94afc3ac75246747448d365
BIT Student <classroom@bgit.rs>
2019-11-29 10:53:37 +0100

    second feature completed

--- bit-people/src/view/Users/UserListItem.js
+++ bit-people/src/view/Users/UserListItem.js
@@ -4,9 +4,9 @@ const UserListItem = props => {
     return (
         <li className="collection-item avatar">
             <img className="circle" src={props.picture}></img>
-            <span className="title">name: {props.first}</span>
-            <p>email: {props.email} </p>
-            <p> date of Birth: {props.dob}</p>
+            <span className="title">name: {props.first} {props.last}</span>
+            <p> <i class="material-icons">email</i> email: {props.email} </p>
+            <p> <i class="material-icons">cake</i>date of Birth: {props.dob}</p>
         </li>
     )
 }
